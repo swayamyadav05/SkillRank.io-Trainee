2c321108b7242beab8505860ccb308a0b49b5420
Mangloo69 <yadav69.swayam@gmail.com>
2024-11-06 23:08:53 +0530

    Angular_Framework

---
+++ Angular/first-ng-app/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+  `,
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
+})
+export class AppComponent {
+  title = 'first-ng-app';
+}
---
+++ Angular/first-ng-app/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [GreetingComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  homeMessage = signal('Hello, world!');
+}
