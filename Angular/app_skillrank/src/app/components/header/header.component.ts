6302f57fa1df7b83284bf46def4df16a0667b2d2
Mangloo69 <yadav69.swayam@gmail.com>
2024-11-09 23:58:45 +0530

    Nav bar

--- Angular/app_skillrank/src/app/components/header/header.component.ts
+++ Angular/app_skillrank/src/app/components/header/header.component.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  title = signal('My First Angular App');
+  logo = signal('SkillRank');
 }
